7d6fcdb480150f565f5a79274840c20df3ed672f
Xiao Du <xiaodu910@gmail.com>
2016-08-14 22:46:59 -0400

    PrototypePattern added

---
+++ src/prototypePattern/Rectangle.java
@@ -0,0 +1,17 @@
+package prototypePattern;
+
+/**
+ * @Author: Xiao Du
+ * @Date: 8/14/2016.
+ */
+public class Rectangle extends Shape {
+
+    public Rectangle() {
+        type = "Rectangle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
