f6383947308a5d9afd97e915f6dc24e3e2074c14
Xiao Du <xiaodu910@gmail.com>
2018-04-28 22:53:26 -0700

    make method private

--- src/behavioralPatterns/interpreterPattern/InterpreterPatternDemo.java
+++ src/behavioralPatterns/interpreterPattern/InterpreterPatternDemo.java
@@ -9,14 +9,14 @@ package behavioralPatterns.interpreterPattern;
 public class InterpreterPatternDemo {
 
     // Rule: Robert and John are male.
-    public static Expression getMaleExpression() {
+    private static Expression getMaleExpression() {
         Expression robert = new TerminalExpression("Robert");
         Expression john = new TerminalExpression("John");
         return new OrExpression(robert, john);
     }
 
     // Rule: Julie is a married women.
-    public static Expression getMarriedWomanExpression() {
+    private static Expression getMarriedWomanExpression() {
         Expression julie = new TerminalExpression("Julie");
         Expression married = new TerminalExpression("Married");
         return new AndExpression(julie, married);
