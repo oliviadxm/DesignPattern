5a834b4cb08de4368fcd730cadf05a00cddae444
Xiao Du <xiaodu910@gmail.com>
2018-05-08 22:24:19 -0700

    iterator pattern commit

---
+++ src/behavioralPatterns/iteratorPattern/Container.java
@@ -0,0 +1,5 @@
+package behavioralPatterns.iteratorPattern;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ src/behavioralPatterns/iteratorPattern/Iterator.java
@@ -0,0 +1,6 @@
+package behavioralPatterns.iteratorPattern;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
