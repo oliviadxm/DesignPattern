769106784603b660795261148b58c0774c09c4c5
Xiao Du <xiaodu910@gmail.com>
2018-06-14 20:34:32 -0700

    add documentation

--- src/behavioralPatterns/nullObjectPattern/NullPatternDemo.java
+++ src/behavioralPatterns/nullObjectPattern/NullPatternDemo.java
@@ -1,5 +1,17 @@
 package behavioralPatterns.nullObjectPattern;
 
+/**
+ * (NO. 22)
+ * In Null Object pattern, a null object replaces check of NULL object instance.
+ * Instead of putting if check for a null value, Null Object reflects a do nothing
+ * relationship. Such Null object can also be used to provide default behaviour in
+ * case data is not available.
+ *
+ * In Null Object pattern, we create an abstract class specifying various operations
+ * to be done, concrete classes extending this class and a null object class providing
+ * do nothing implementation of this class and will be used seemlessly where we need
+ * to check null value.
+ */
 public class NullPatternDemo {
 
     public static void main(String[] args) {
